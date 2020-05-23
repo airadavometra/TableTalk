f71c3d6d62c8bd08db1ec5cd5486fdeefe6f73c3
dsartem5 <dsartem5@mts.ru>
2020-05-23 16:39:30 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TableTalk.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+		public int QuestionsCount;
+		public void OnGet()
+		{
+			string URL = "https://tabletalkwebapi.azurewebsites.net";
+
+			HttpClient client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true })
+			{
+				BaseAddress = new Uri(URL)
+			};
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			var path = $"{URL}/questions";
+
+			var response = client.GetAsync(path);
+
+			QuestionsCount = int.Parse(response.Result.Content.ReadAsStringAsync().Result);
+		}
+	}
+}
